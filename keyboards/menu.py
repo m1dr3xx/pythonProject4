06a91c3c5986ea9a33bb80f8006bcf70a884dc36
sum52 <m1dr3xx@gmail.com>
2023-07-11 13:43:07 +0700

    коты и собаки с меню

---
+++ keyboards/menu.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+main_menu_command = [
+    BotCommand(command='/start', description='Перезапустить'),
+    BotCommand(command='/help', description='Помощь')
+]
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command, Text
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message
 
 from config import config  # Config
+from keyboards.menu import main_menu_command
 from keyboards.reply import cats_dogs_keyboard
 
 API_TOKEN = config.token
@@ -21,19 +22,29 @@ async def start_command(message: Message):
 
 @dp.message(Text(text='Собак'))
 async def handle_dogs_answer(message: Message):
-    await message.answer('Ого, мне тоже. Обожаю их',
-                         reply_markup=ReplyKeyboardRemove())
+    await message.answer('Ого, мне тоже. Обожаю их')
 
 
 @dp.message(Text(text='Котов'))
 async def handle_cats_answer(message: Message):
-    await message.answer('Интересный выбор. Но мне не нравится',
-                         reply_markup=ReplyKeyboardRemove())
+    await message.answer('Интересный выбор. Но мне не нравится')
+
+
+@dp.message(Command(commands='help'))
+async def handle_help(message: Message):
+    await message.answer('Типа помощь')
+
+
+@dp.message(Command('delete_menu'))
+async def handle_menu_delete(message: Message, bot: Bot):
+    await bot.delete_my_commands()
+    await message.answer('Вы удалили меню(((')
 
 
 async def main():
     try:
         print('Bot Started')
+        await bot.set_my_commands(main_menu_command)
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
